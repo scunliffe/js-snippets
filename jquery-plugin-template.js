b4b1b4883e52c1d5889acbbd1ab7d751a8231629
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2015-04-04 08:42:12 -0400

    added options and chainability

--- jquery-plugin-template.js
+++ jquery-plugin-template.js
@@ -1,5 +1,13 @@
 ;(function($, window, undefined){
-	$.fn.myPlugin = function(){
-		//add your stuff here
+	$.fn.myPlugin = function(options){
+		var settings = $.extend({
+			'foo':123,
+			'bar':456
+		}, options);
+		
+		//plugin code, returns 'this' (the jQuery selector to maintain chainability)
+		return this.each(function(){        
+			//add your stuff here	
+		});
 	};
 }(jQuery, window));
