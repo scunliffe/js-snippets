43c45e3f25f89c4d9da6cfb71e0162c48dc1f764
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2015-09-05 17:02:39 -0400

    added extendNamespace method

--- utils.js
+++ utils.js
@@ -4,3 +4,18 @@ function randomNumber(min, max){
 	var rnd = Math.floor((Math.random()*((max+1)-min))+min);
 	return rnd;
 }
+
+function extendNamespace(ns, nsStr){
+	var bits = nsStr.split('.');
+	var parent = ns;
+	var key;
+	var bL = bits.length;
+	for(var i=0;i<bL;i++){
+		key = bits[i];
+		if(typeof(parent[key]) == 'undefined'){
+			parent[key] = {};
+		}
+		parent = parent[key];
+	}
+	return parent;
+}
