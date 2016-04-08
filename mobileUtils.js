5f542b25a68cd061f4fc54283582dd7065826699
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2016-04-08 17:27:47 -0400

    Added pixel factor and scrolling adjustments

--- mobileUtils.js
+++ mobileUtils.js
@@ -7,24 +7,32 @@ var phones = {
 		'deviceWidth':320,
 		'deviceHeight':480,
 		'browserWidth':320,
-		'browserHeight':372
+		'browserHeight':372,
+		'extraHeightDuringScroll':68,
+		'renderedPixelsFactor':2
 	},
 	'iPhone5':{
 		'deviceWidth':320,
 		'deviceHeight':568,
 		'browserWidth':320,
-		'browserHeight':460
+		'browserHeight':460,
+		'extraHeightDuringScroll':68,
+		'renderedPixelsFactor':2
 	},
 	'iPhone6':{
 		'deviceWidth':375,
 		'deviceHeight':667,
 		'browserWidth':375,
-		'browserHeight':559
+		'browserHeight':559,
+		'extraHeightDuringScroll':68,
+		'renderedPixelsFactor':2
 	},
 	'iPhone6plus':{
 		'deviceWidth':414,
 		'deviceHeight':736,
 		'browserWidth':414,
-		'browserHeight':628
+		'browserHeight':628,
+		'extraHeightDuringScroll':68,
+		'renderedPixelsFactor':3
 	},
 };
