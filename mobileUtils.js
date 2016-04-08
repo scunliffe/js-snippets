929d7e945483efc9d7c461bbc9ac98b3361a6a53
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2016-04-08 17:23:46 -0400

    Created mobileUtils.js

---
+++ mobileUtils.js
@@ -0,0 +1,30 @@
+//Mobile (phone/tablet) screen sizes
+//reference data for iPhone from: http://www.kylejlarson.com/blog/iphone-6-screen-size-web-design-tips/
+
+//in portrait mode
+var phones = {
+	'iPhone4':{
+		'deviceWidth':320,
+		'deviceHeight':480,
+		'browserWidth':320,
+		'browserHeight':372
+	},
+	'iPhone5':{
+		'deviceWidth':320,
+		'deviceHeight':568,
+		'browserWidth':320,
+		'browserHeight':460
+	},
+	'iPhone6':{
+		'deviceWidth':375,
+		'deviceHeight':667,
+		'browserWidth':375,
+		'browserHeight':559
+	},
+	'iPhone6plus':{
+		'deviceWidth':414,
+		'deviceHeight':736,
+		'browserWidth':414,
+		'browserHeight':628
+	},
+};
