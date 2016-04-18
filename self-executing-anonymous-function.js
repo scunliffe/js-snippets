0d9fad1f8ac76525ba8956417f032a02cd70a27d
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2016-04-18 09:45:16 -0400

    Created self-executing-anonymous-function.js

---
+++ self-executing-anonymous-function.js
@@ -0,0 +1,14 @@
+(function(example, $, undefined){
+	var privateVariable = 'privateValue';
+
+	example.publicVariable = 'publicValue';
+
+	function privateMethod(){
+		
+	}
+
+	example.publicMethod = function(){
+		
+	};
+
+})(window.example = window.example || {}, jQuery);
