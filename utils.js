06a28c549b21889555b83dd0773f8a2f0d9cc811
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2018-12-13 10:04:31 -0500

    Added getUUID4

--- utils.js
+++ utils.js
@@ -20,7 +20,31 @@ function extendNamespace(ns, nsStr){
 	return parent;
 }
 
-//
 function getAsConstant(val){
 	return function(){return val;};
+}
+
+//get arandom UUID in the format xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+function getUUID4(){
+	var uuid = '', i;
+	for(i=0;i<32;i+=1){
+		switch(i){
+		case 8:
+		case 20:
+			uuid += '-';
+			uuid += (Math.random() * 16 | 0).toString(16);
+			break;
+		case 12:
+			uuid += '-';
+			uuid += '4';
+			break;
+		case 16:
+			uuid += '-';
+			uuid += (Math.random() * 4 | 8).toString(16);
+			break;
+		default:
+			uuid += (Math.random() * 16 | 0).toString(16);
+		}
+	}
+	return uuid;
 }
